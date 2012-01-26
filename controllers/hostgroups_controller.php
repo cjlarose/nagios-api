5c6669f23c3301ea419abb5923095e90b92240d5
Chris La Rose <cjlarose@iplantcollaborative.org>
2012-01-26 11:29:04 -0500

    Renamed HostGroup to Hostgroup in hostgroup controller

--- controllers/hostgroups_controller.php
+++ controllers/hostgroups_controller.php
@@ -2,11 +2,11 @@
 class HostgroupsController extends Controller {
 
 	function index() {
-		$host_groups = HostGroup::find('all');
+		$host_groups = Hostgroup::find('all');
 		$result_string = "[";
 		$host_json_array = array();
 		foreach ($host_groups as $host_group)
-			$host_json_array[] = $host_group->to_json(array('include' => array('hosts')));
+			$host_json_array[] = $host_group->to_json(array('include' => array('hostgroup_members', 'hosts')));
 		$result_string .= implode(',', $host_json_array);
 		$result_string .= "]";
 		$this->send_response(200, $result_string, 'application/json');
